08eb85ff0a262125e462ca6c769b658c0f7f43e3
AgamSingh <32994947+AgamSingh@users.noreply.github.com>
2017-10-28 02:11:26 +0530

    Add files via upload

---
+++ breaking_RAD_1.ino
@@ -0,0 +1,150 @@
+//Fingerprint
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+
+int getFingerprintIDez();
+
+SoftwareSerial mySerial(2, 3);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+
+//GPS
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial serial_connection(10, 11); //RX=pin 11, TX=pin 10 ,Tx 10 mein Rx 11 mein
+
+TinyGPSPlus gps;
+
+
+void setup()  
+{
+  //Fingerprint
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  
+  Serial.begin(9600);
+  Serial.println("Adafruit finger detect test");
+
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+  Serial.println("Waiting for valid finger...");
+
+
+  //GPS
+  Serial.begin(9600);
+  serial_connection.begin(9600);
+  Serial.println("GPS Start");
+}
+
+void loop()                     
+{
+  getFingerprintIDez();
+  delay(50);           
+}
+
+uint8_t getFingerprintID() {
+
+
+  //Fingerprint
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  //success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  //converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  return finger.fingerID; 
+
+
+//GPS
+while(serial_connection.available())
+  {
+    gps.encode(serial_connection.read());
+  }
+  if(gps.location.isUpdated())
+  {
+    
+    Serial.println("Latitude:");
+    Serial.println(gps.location.lat(), 6);
+    Serial.println("Longitude:");
+    Serial.println(gps.location.lng(), 6);
+    
+    Serial.println("");
+  }
+}
